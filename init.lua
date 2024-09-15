28f1355bc4997b92cb9e60b83bc52063e1aee34f
Tokarev Artem <tokarev28.art@gmail.com>
2024-09-15 18:45:11 +0500

    add Nix formatter

--- init.lua
+++ init.lua
@@ -609,7 +609,15 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         rust_analyzer = {},
-        nil_ls = {},
+        nil_ls = {
+          settings = {
+            ['nil'] = {
+              formatting = {
+                command = { 'nixfmt' },
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
